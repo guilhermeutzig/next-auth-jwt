78b990bf318857bfbfaaad5b67c0f0355e89507c
Guilherme Utzig <guilhermelutzig@gmail.com>
2023-03-20 11:23:54 -0300

    initial setup

---
+++ src/pages/signin.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Sign in</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <form>
+          <h1>Sign In</h1>
+          <input />
+        </form>
+      </main>
+    </>
+  );
+}
